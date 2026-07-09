beb553d93f3a8982417d6683b28aaf2c4786526c
Keshav Sharma <keshavsharma74478@gmail.com>
2026-07-09 12:08:30 +0530

    Update dialog_share_subject.py

--- ai-attendance-project-app-main/src/components/dialog_share_subject.py
+++ ai-attendance-project-app-main/src/components/dialog_share_subject.py
@@ -6,7 +6,7 @@ import io
 
 @st.dialog("Share Class Link")
 def share_subject_dialog(subject_name, subject_code):
-    app_domain = "https://attendnova.streamlit.app"
+    app_domain = "https://attend-nova.streamlit.app"
     join_url = f"{app_domain}/?join-code={subject_code}"
 
     st.header("Scan to Join")
